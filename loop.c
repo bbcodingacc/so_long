fc1c42cf6761aa6a335332f51ea24ccb1889adce
Metehan Karaboga <mkarabog@k1m08s07.42istanbul.com.tr>
2023-06-18 20:11:35 +0300

    2.1 wall control added

--- loop.c
+++ loop.c
@@ -21,32 +21,53 @@ int keyboard(int keycode, t_data *s_data)
         return (0);
 }
 
-int move(int keycode, t_data *s_data)
+int wall_control(t_data *s_data, int x, int y)
+{
+	if(s_data->map[y][x] == '1')
+		return (1);
+	else
+		return (0);
+}
+
+int move_y(int keycode, t_data *s_data)
 {
 	int		x;
 	int		y;
-	char	c;
+	int		swap;
 
 	x = s_data->player_x;
 	y = s_data->player_y;
-	if(keycode == 119 || keycode == 13 || keycode == 115 || keycode == 1)
-	{
-		s_data->map[y][x] = '0';
-		if(keycode == 115 || keycode == 1)
-			y = y + 1;
-		else
-			y = y - 1;
-		s_data->map[y][x] = 'P';
-	}
-	else if(keycode == 100 || keycode == 2 || keycode == 97 || keycode == 0)
-	{
-		s_data->map[y][x] = '0';
-		if(keycode == 97 || keycode == 0)
-			x = x - 1;
-		else
-			x = x + 1;
-		s_data->map[y][x] = 'P';
-	}
+	swap = y;
+	if(keycode == 1)
+		y = y + 1;
+	else
+		y = y - 1;
+	if(wall_control(s_data, x, y) == 1)
+		return (0);
+	s_data->map[swap][x] = '0';
+	s_data->map[y][x] = 'P';
+	s_data->player_x = x;
+	s_data->player_y = y;
+	return (1);
+}
+
+int move_x(int keycode, t_data *s_data)
+{
+	int		x;
+	int		y;
+	int		swap;
+
+	x = s_data->player_x;
+	y = s_data->player_y;
+	swap = x;
+	if(keycode == 0)
+		x = x - 1;
+	else
+		x = x + 1;
+	if(wall_control(s_data, x, y) == 1)
+		return (0);
+	s_data->map[y][swap] = '0';
+	s_data->map[y][x] = 'P';
 	s_data->player_x = x;
 	s_data->player_y = y;
 	return (1);
@@ -54,9 +75,11 @@ int move(int keycode, t_data *s_data)
 
 int action(int keycode, t_data *s_data)
 {
-
 	keyboard(keycode, s_data);
-	move(keycode, s_data);
+	if(keycode == 13 || keycode == 1)
+		move_y(keycode, s_data);
+	else if(keycode == 2 || keycode == 0)
+		move_x(keycode, s_data);
 	mlx_clear_window(s_data->mlx_ptr, s_data->win_ptr);
 	get_line_array(s_data);
     return (0);
